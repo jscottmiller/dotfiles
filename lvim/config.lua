b0ca3c68ce808b5764e6b10cabcffa634467067f
J. Scott Miller <j.scott.miller@gmail.com>
2022-02-11 15:46:37 -0600

    Disables projects.

--- lvim/config.lua
+++ lvim/config.lua
@@ -36,7 +36,7 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- end
 
 -- Disable project plugin by default
-lvim.builtin.project.active=false
+--lvim.builtin.project.active=false
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
