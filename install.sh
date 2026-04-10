20df6bb51d27a5d2f33eb398f170be509f0aeb93
Scott Miller <scott@coder.com>
2026-04-09 20:31:43 -0500

    Fix AstroNvim version pin to ^6, add git include wiring to install script.

--- install.sh
+++ install.sh
@@ -15,6 +15,11 @@ fi
 ln -sf "$DOTFILES_DIR/.tmux.conf" "$HOME/.tmux.conf"
 ln -sf "$DOTFILES_DIR/.gitconfig.common" "$HOME/.gitconfig.common"
 
+# Wire up git include if not already present
+if ! git config --global --get-all include.path | grep -qF .gitconfig.common; then
+  git config --global --add include.path "$HOME/.gitconfig.common"
+fi
+
 # AstroNvim config
 mkdir -p "$HOME/.config"
 rm -rf "$HOME/.config/nvim"
